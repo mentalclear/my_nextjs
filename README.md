f949b3c35e5db62d437ebfa140f1099b80fe9b7a
Dmitrii Kilishek <mentalclear@gmail.com>
2021-09-18 17:54:11 -0700

    Continue with Pre-rendering and Data Fetching

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ## My repo for Next.js tutorial progress
 
+- Tutorial location: 
+https://nextjs.org/learn/basics/create-nextjs-app
---
+++ nextjs-blog/pages/_app.js
@@ -0,0 +1,8 @@
+// A file that imports Global CSS files. 
+// The only location to be used for that purpose
+import '../styles/global.css'
+
+
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />
+}
